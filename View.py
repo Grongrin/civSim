6c93b4f5c2f4341da39caa1354496abaf73b7e24
Grongrin <ulfnitjsefin@gmail.com>
2019-01-07 18:45:42 +0100

    viev update

--- View.py
+++ View.py
@@ -2,15 +2,25 @@ import TileMap as tm
 from tkinter import *
 import math
 
+class Tile:
+    def __init__(self,x,y,a,cords,index):
+        self.x_ = x
+        self.y_= y
+        self.a_ = a
+        self.cords_ = cords
+        self.index_=index
 
 class View(Tk):
     def __init__(self):
         Tk.__init__(self)
+        self.hexagons = []
+        self.biggestTileX=0
+        self.biggestTileY=0
+        self.infoId=0
         self.canvas = Canvas(width=1400, height=900,bg="#AFEEEE" )
         self.canvas.pack()
         self.canvas.bind("<Motion>", self.getTileByXY)
-
-
+        self.canvas.create_text(1100,10,text="Tile info:")
 
     def drawMap(self,map):
         xSize = map.getXSize()
@@ -19,65 +29,98 @@ class View(Tk):
             yHeight = ySize*1.5
         else:
             yHeight = math.ceil(ySize/2)*2+ySize//2
-
-
         xHeight = (2*xSize+1)*math.sqrt(3)/2
         print(yHeight, xHeight)
-        a=20
-        allTiles=map.getMap()
+        if yHeight>xHeight:
+            a=1100/yHeight*0.9
+        else:
+            a=900/xHeight*0.9
 
+        allTiles=map.getMap()
         for i in range(xSize):
             for j in range(ySize):
                 self.drawTile(allTiles[i][j],a)
 
-    def drawTile(self, tile, a):
+    def drawTile(self,tile,a):
         tileCoords = tile.getCoords()
-        if tileCoords[1] % 2==0:
-            startX= 20 + a * math.sqrt(3) * tileCoords[0]
+        if tileCoords[1] % 2 == 0:
+            startX = 1.5*a + a * math.sqrt(3) * tileCoords[0]
         else:
-            startX = 20 + a * math.sqrt(3) / 2 + a * math.sqrt(3) * tileCoords[0]
-        startY = 30 + 1 / 2 * a + 1.5 * tileCoords[1] * a
+            startX = 1.5*a + a * math.sqrt(3) / 2 + a * math.sqrt(3) * tileCoords[0]
+        startY = 1.5*a + 1 / 2 * a + 1.5 * tileCoords[1] * a
         angle = 60
         coords = []
-        for i in range(1,7):
+        for i in range(1, 7):
             coords.append([startX, startY])
-            endX = startX +  a * math.sin(math.radians(angle * i))
-            endY = startY +a * math.cos(math.radians(angle * i))
+            endX = startX + a * math.sin(math.radians(angle * i))
+            endY = startY + a * math.cos(math.radians(angle * i))
             startX = endX
             startY = endY
         if tile.getType() == 1:
             color = "#FFFF66"
         else:
             color = "#3366FF"
-        self.canvas.create_polygon(coords[0][0],
-                                       coords[0][1],
-                                       coords[1][0],
-                                       coords[1][1],
-                                       coords[2][0],
-                                       coords[2][1],
-                                       coords[3][0],
-                                       coords[3][1],
-                                       coords[4][0],
-                                       coords[4][1],
-                                       coords[5][0],
-                                       coords[5][1],
-                                       fill=color,
-                                       outline="gray",
-                                       )
+        index = self.canvas.create_polygon(coords[0][0],
+                                   coords[0][1],
+                                   coords[1][0],
+                                   coords[1][1],
+                                   coords[2][0],
+                                   coords[2][1],
+                                   coords[3][0],
+                                   coords[3][1],
+                                   coords[4][0],
+                                   coords[4][1],
+                                   coords[5][0],
+                                   coords[5][1],
+                                   fill=color,
+                                   outline="gray",
+                                   )
+
+        for i in coords:
+            if i[0]>self.biggestTileX:
+                self.biggestTileX = i[0]
+            if i[1]>self.biggestTileY:
+                self.biggestTileY=i[1]
+        print("(",tile.getX(), ", ", tile.getY(),", ",index, ")", sep='')
+
+        hex = Tile(tile.getX(),tile.getY(),a,coords,index)
+        self.hexagons.append(hex)
 
 
-    def showTileDetails(x,y):
-        print("(",x, ", ", y, ")", sep='')
+    def showTileDetails(self,tile):
+        if tile==None:
+            return
+        self.canvas.delete(self.infoId)
+        onscreen = "("+str(tile.x_)+","+str(tile.y_)+")"
+        self.infoId=self.canvas.create_text(1100,20,text=onscreen)
+        print("(",tile.x_, ", ", tile.y_, ")", sep='')
+
+
+    def findNearestTile(self,nearest):
+        for i in self.hexagons:
+            if i.index_ == nearest:
+                return i
+        return None
 
     def getTileByXY(self,event):
         print(event.x,event.y)
-        self.canvas.find_closest(event.x,event.y)
+        if (event.x<self.biggestTileX and event.y<self.biggestTileY):
+            nearest = int(self.canvas.find_closest(event.x,event.y)[0])
+            print(nearest)
+            tile = self.findNearestTile(nearest)
+            if tile:
+                self.showTileDetails(tile)
+
 
 if __name__ =='__main__':
     view = View()
-    map = tm.TileMap(7,7)
-    map.getTile(3,3).type = 0
+    map = tm.TileMap(100,170)
+    m = map.getMap()
+    for i in m:
+        for j in i:
+            j.printCoords()
     view.drawMap(map)
+    print(len(view.hexagons))
     view.mainloop()
 
 
